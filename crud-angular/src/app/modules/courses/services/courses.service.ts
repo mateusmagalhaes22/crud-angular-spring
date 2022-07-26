3f8638d3bdbd5fac4a10b93f32a1a1f3058f6b52
Mateus Magalhães <magalhaes.mateus22@gmail.com>
2022-07-26 19:53:23 -0300

    loading-spinner adicionado

--- crud-angular/src/app/modules/courses/services/courses.service.ts
+++ crud-angular/src/app/modules/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 import { Course } from './../model/course';
 
@@ -16,6 +16,8 @@ export class CoursesService {
     list(){
       return this.httpClient.get<Course[]>(this.API)
       .pipe(
+        first(),
+        delay(500),
         tap(courses => console.log(courses))
       );
     }
